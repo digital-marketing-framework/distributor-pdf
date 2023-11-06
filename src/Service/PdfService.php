4e6cab018cd668b7b9b185c00c50ff42d85fc7ce
Meelis Karulin <karulin@mediatis.de>
2023-11-06 16:57:35 +0100

    [BUGFIX] Use DMF FileStorage class to handle reading and writing to files (where possible)
    Both pdfOutputDir and pdfTemplatePath must now be DMF FileStorage readable file identifiers

--- src/Service/PdfService.php
+++ src/Service/PdfService.php
@@ -2,41 +2,50 @@
 
 namespace DigitalMarketingFramework\Distributor\Pdf\Service;
 
+use DigitalMarketingFramework\Core\FileStorage\FileStorageAwareInterface;
+use DigitalMarketingFramework\Core\FileStorage\FileStorageAwareTrait;
+use DigitalMarketingFramework\Typo3\Core\Utility\VendorAssetUtility;
 use Exception;
 use FPDM;
 
-class PdfService
+class PdfService implements FileStorageAwareInterface
 {
+
+    use FileStorageAwareTrait;
+
     /**
      * @param array<mixed> $settings
      *
      * @return array<mixed>|bool
      */
-    public function generatePdf($settings)
+    public function generatePdf(array $settings): array|bool
     {
-        if (!is_array($settings['pdfFormFields']) || $settings['pdfOutputDir'] == '' || $settings['pdfOutputName'] == '' || $settings['pdfTemplatePath'] == '' || !file_exists($settings['pdfTemplatePath'])) {
+        if (!is_array($settings['pdfFormFields']) || $settings['pdfOutputDir'] == '' || $settings['pdfOutputName'] == '' || $settings['pdfTemplatePath'] == '' || $settings['pdfTemplatePath'] == '') {
             return false;
         }
 
         $processedFields = $settings['pdfFormFields'];
 
-        $tempDir = $this->createUniqueTempDirectory($settings['pdfOutputDir']);
-        if (!$tempDir) {
-            return false;
-        }
-
-        $generatedPdf = $tempDir . '/' . $settings['pdfOutputName'];
+        $templateContents = $this->fileStorage->getFileContents($settings['pdfTemplatePath']);
+        $tempFile = $this->fileStorage->writeTempFile('', $templateContents, '.pdf');
 
         try {
-            $pdf = new FPDM($settings['pdfTemplatePath']); // @phpstan-ignore-line because the FPDM constructor gets its arguments via func_get_args()
+            $pdf = new FPDM($tempFile); // @phpstan-ignore-line because the FPDM constructor gets its arguments via func_get_args()
             if ($settings['useCheckboxParser']) {
                 $pdf->useCheckboxParser = true;
             }
 
             $pdf->Load($processedFields, true);
             $pdf->Merge();
-            $pdf->Output('F', $generatedPdf);
-
+            $pdf->Output('F', $tempFile);
+            $mergedContent = file_get_contents($tempFile);
+            $uniqueOutputDir = $this->createUniqueDirectory($settings['pdfOutputDir']);
+            if (!$uniqueOutputDir) {
+                return false;
+            }
+            $generatedPdf = $uniqueOutputDir . '/' . $settings['pdfOutputName'];
+            $this->fileStorage->putFileContents($generatedPdf, $mergedContent);
+            unlink($tempFile);
             return ['fileName' => $settings['pdfOutputName'], 'publicUrl' => $generatedPdf, 'relativePath' => $generatedPdf, 'mimeType' => mime_content_type($generatedPdf)];
         } catch (Exception) {
             // TODO: REALLY catch errors like "FPDF-Merge Error: field companyname not found"
@@ -49,23 +58,23 @@ class PdfService
      *
      * @return string|bool
      */
-    private function createUniqueTempDirectory(string $dir, int $maxTries = 500)
+    private function createUniqueDirectory(string $dir, int $maxTries = 500)
     {
-        if (!file_exists($dir)) {
-            mkdir($dir);
+        if (!$this->fileStorage->folderExists($dir)) {
+            $this->fileStorage->createFolder($dir);
         }
 
         $dir .= uniqid('pdf_') . '_';
         $tries = 0;
-        while ($tries < $maxTries && file_exists($dir . $tries)) {
+        while ($tries < $maxTries && $this->fileStorage->folderExists($dir . $tries)) {
             ++$tries;
         }
 
         $dir .= $tries;
-        if (!file_exists($dir) && mkdir($dir)) {
+        $this->fileStorage->createFolder($dir);
+        if ($this->fileStorage->folderExists($dir)) {
             return $dir;
         }
-
         return false;
     }
 }
